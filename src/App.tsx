5e5656891792abaa1b11ca55bf5bb0d1da77f903
J3richoDev <nomat.dev@gmail.com>
2025-01-22 11:04:56 +0800

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { MantineProvider } from '@mantine/core'
+
+function App() {
+  return (
+    <>
+      <MantineProvider>
+      <h1 className="text-3xl font-bold underline">
+      Halo Zawardu
+      </h1>
+      </MantineProvider>
+    </>
+  )
+}
+
+export default App
